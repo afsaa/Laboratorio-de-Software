dd6a678a657d4159b0537490a4e957538102a47e
David Tovar Martinez <eltover@gmail.com>
2017-09-20 12:19:31 -0500

    test profile

--- tiendaonline/profiles/tests.py
+++ tiendaonline/profiles/tests.py
@@ -2,5 +2,36 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from profiles.models import profile
 
 # Create your tests here.
+class TestProfiles(TestCase):
+    def test_myperfil_str(self):
+        myperfil = profile(name='Chéri')
+        self.assertEqual(str(myperfil), 'Chéri'.encode('utf8'))
+
+    def test_myperfil_str2(self):
+        myperfil = profile(name='fa21er')
+        self.assertEqual(str(myperfil), 'Chéri'.encode('utf8'))
+
+    def test_myperfil_str3(self):
+        myperfil = profile(name='fa21er')
+        self.assertEqual(str(myperfil), profile.name)
+
+    def test_myperfil_str4(self):
+    	profile.objects.create(name="heynow")
+        myperfil = profile(name='heynow')
+        self.assertEqual(str(myperfil), "heynow")             
+
+
+    def test_myperfil_telefono(self):
+        myperfil = profile(telefono='3113032015')
+        self.assertTrue(True)
+
+        myperfil2 = profile(telefono='+3158889498')
+        self.assertEqual(myperfil2, myperfil)  
+
+    
+
+   
+
