b61e8a1abc1ca91623874d8cd57ab263e97bc93e
David Tovar Martinez <eltover@gmail.com>
2017-09-20 07:34:53 -0500

    modificacion de modelo perfiles

--- tiendaonline/profiles/models.py
+++ tiendaonline/profiles/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.core.validators import RegexValidator
+from django.core.validators import EmailValidator
 from django.db import models
 
 # Create your models here.
@@ -9,7 +10,9 @@ class profile(models.Model):
 	name = models.CharField(max_length=120)
 	description = models.TextField(default='description default text')	
 	regla_telefono = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="El formato de telefono es: '+999999999'. maximo 15 digitos permitido.")
-	telefono = models.CharField(validators=[regla_telefono], max_length=15, blank=True) 
+	telefono = models.CharField(validators=[regla_telefono], max_length=15, blank=True)
+	email = models.EmailField(blank=False, validators=[EmailValidator(message="Ingresa un email valido")], default='description email')
+	direccion = models.CharField(max_length=120, default='description address text')
 
 	def __unicode__(self):
 		return self.name
