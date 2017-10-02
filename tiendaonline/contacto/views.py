d6127354c08d42da132c6cab70a754c7e337294a
David Tovar Martinez <eltover@gmail.com>
2017-10-01 21:14:39 -0500

    enviando emails

--- tiendaonline/contacto/views.py
+++ tiendaonline/contacto/views.py
@@ -1,13 +1,28 @@
 
 from django.shortcuts import render
 
+from django.core.mail import send_mail
+from django.conf import settings
+
 from .forms import contactForm
-# Create your views here.
+
 def contacto(request):
+	title='Contacto'
 	form = contactForm(request.POST or None)
+	context = {'title': title, 'form':form, }
 
 	if form.is_valid():
-		print form.cleaned_data['email']
-	context = locals()
+		name = form.cleaned_data['nombre']
+		comment = form.cleaned_data['comentario']
+		print  form.cleaned_data['email']
+		subject = 'Mensaje de SexShop.com'
+		message = '%s %s' %(comment, name)
+		emailFrom = form.cleaned_data['email']
+		emailTo = [settings.EMAIL_HOST_USER]
+		send_mail(subject, message, emailFrom, [emailFrom, 'sexshoplab@gmail.com'], fail_silently=False)
+		title="Gracias !"
+		confirm_message ="El mensaje ha sido enviado, te lo responderemos en breve. gracias por tu paciencia."
+		context = {'title': title, 'confirm_message':confirm_message, }
+	
 	template = 'contacto.html'
 	return render(request,template,context)
